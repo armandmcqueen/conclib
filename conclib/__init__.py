5280ed6dc1968b43ee683dcb92f28f1a0f3c32d2
Armand McQueen <armandmcqueen@gmail.com>
2023-12-12 19:41:46 -0500

    Fix circular imports

--- conclib/__init__.py
+++ conclib/__init__.py
@@ -1,15 +1,17 @@
-from conclib import constants  # noqa: F401
-from conclib.config import ConclibConfig, DefaultConfig  # noqa: F401
+# NOTE: Order of imports is important here to avoid circular imports
 
+from conclib.config import ConclibConfig, DefaultConfig  # noqa: F401
+from conclib import constants  # noqa: F401
+from conclib.proxy.messages import ActorMessage  # noqa: F401
+from conclib import errors  # noqa: F401
 from conclib.pykka_extensions.actor import Actor  # noqa: F401
 from conclib.pykka_extensions.ticker import Ticker  # noqa: F401
-
-from conclib.proxy.actor import start_proxy  # noqa: F401
 from conclib.proxy.client import ProxyClient  # noqa: F401
-from conclib.proxy.messages import ActorMessage  # noqa: F401
+
 from conclib.proxy.envelope import RequestEnvelope, ResponseEnvelope  # noqa: F401
+from conclib.proxy.actor import start_proxy  # noqa: F401
 
 from conclib.utils.redisd.redisserverd import start_redis  # noqa: F401
 from conclib.utils.apid.apid import start_api  # noqa: F401
 
-from conclib import errors  # noqa: F401
+
